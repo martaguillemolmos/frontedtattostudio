3b79e36f9d08b09313bbeed9346dd362c984af95
martaguillemolmos <martetaguillem@gmail.com>
2023-11-09 17:27:42 +0100

    feat: Work on the template that we have developed in the modelReact repository. If we want to view the commits, we just have to go to that repository.

---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
+import { Home } from '../Home/Home'
+import { Register } from '../Register/Register'
+import { Login } from '../Login/Login'
+import { Profile } from '../Profile/Profile'
+
+export const Body = () => {
+    return (
+        <>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/profile' element={<Profile />}/>
+        </Routes>
+        </>
+    )
+}
