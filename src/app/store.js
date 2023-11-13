020d00b100e66fcbc15deccfa85b0fb083d32619
martaguillemolmos <martetaguillem@gmail.com>
2023-11-13 20:37:24 +0100

    feat:Link the document that we have generated, to which we have called, userSlice in store.js.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import userSlice from '../pages/userSlice';
+import userSlice from '../pages/userSlice';
 // import searchSlice from '../pages/searchSlice';
 
 import { combineReducers } from 'redux';
@@ -10,7 +10,7 @@ import { persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
-    // user: userSlice,
+    user: userSlice,
     // search: searchSlice
 })
 
