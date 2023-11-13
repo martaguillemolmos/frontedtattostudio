881b0f80600eb20894c582654e1b4287fc7584cf
martaguillemolmos <martetaguillem@gmail.com>
2023-11-13 20:39:25 +0100

    feat:Import redux and wrap our app in the main.jsx document.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import '@mantine/core/styles.css'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "@mantine/core/styles.css";
+import "./index.css";
 
-import {BrowserRouter} from "react-router-dom"
-import { MantineProvider, createTheme } from '@mantine/core';
+import { BrowserRouter } from "react-router-dom";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+//REDUX
+
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <MantineProvider theme={theme}>
-      <App />
-      </MantineProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <MantineProvider theme={theme}>
+          <App />
+        </MantineProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
