74342af8d1497c9795f5407dd95e32e87007ff5e
martaguillemolmos <martetaguillem@gmail.com>
2023-11-14 18:16:35 +0100

    feat: Inside Regsiter.jsx what we do is retrieve the credentials that we have stored in userSlice. If there are credentials, it means that they have already been logged in. In this case, it does not allow us to access the register view.

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
 import "./Register.css"
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { userData } from "../userSlice";
 
+
+  
 export const Register = () => {
+    const navigate = useNavigate();
+    const rdxCredentials = useSelector(userData);
+
+    
+  //Utilizamos este useEffect para que, en el caso que alguien ya se haya logeado, no pueda acceder a esta vista.
+  useEffect(()=>{
+    //Comprobamos si ya hay un token almacenado en Redux
+        if(rdxCredentials?.credentials.token){
+          //Si ya contamos con un token, redirigimos al usuario a inicio.
+          navigate("/");
+        } 
+      },[rdxCredentials, navigate]);
+
     return (
         <div className="registerDesign">
         Register
