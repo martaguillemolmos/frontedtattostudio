eb76a11ea558ddbe9e0d749ef3bb58c4a38eb114
martaguillemolmos <martetaguillem@gmail.com>
2023-11-15 16:52:04 +0100

    feat: Retrieve the user's token.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,21 @@
 import "./Profile.css"
+import { useEffect } from "react";
+
+// //Importamos Redux
+import {  useSelector } from "react-redux";  
+import { userData } from "../userSlice";
+
 
 export const Profile = () => {
+    // Instanciamos Redux en lectura
+    const rdxToken = useSelector (userData);
+
+    useEffect(() => {
+        // De aquí obtenemos el token.
+        console.log(`nos trae`, rdxToken.credentials.token);
+    }, [rdxToken]);
+    
+
     return (
         <div className="profileDesign">
         Profile
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -12,8 +12,18 @@ export const logUser = async (body) => {
     return await axios.post (`http://localhost:4000/user/login`, body) 
 }
 
+//Profile
+export const profileUser = (token) => {
+    return axios.get (`http://localhost:4000/user/profile`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+}
+
 // Productos
 export const getAllproducts = async () => {
     // Conectamos la API a la base de datos
     return await axios.get (`http://localhost:4000/product`);
 }
+
