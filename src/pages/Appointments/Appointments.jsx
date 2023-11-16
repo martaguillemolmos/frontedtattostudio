c714ed70622ed8313a7e268d9a4ff1321ed960e1
martaguillemolmos <martetaguillem@gmail.com>
2023-11-16 16:29:46 +0100

    feat: Limit, now, if a user does not have a token they cannot access the appointment view.

--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import "./Appointments.css"
+import { useSelector } from "react-redux";
+import { userData } from "../userSlice";
+import { useEffect } from "react";
 
 
 export const Appointments = () => {
+    //Declaramos esta constante para que nos permita dirigirnos desde esta vista a otras.
+    const navigate = useNavigate();
+
+    // Instanciamos Redux en lectura
+     const rdxToken = useSelector(userData);
+
+      //Utilizamos este useEffect para que, en el caso que alguien ya se haya logeado, no pueda acceder a esta vista.
+        useEffect(()=>{
+        //Comprobamos si ya hay un token almacenado en Redux
+        if(!rdxToken?.credentials.token){
+          console.log(rdxToken)
+          //Si ya contamos con un token, redirigimos al usuario a inicio.
+          navigate("/");
+        } 
+      },[rdxToken, navigate]);
+
     return (
         <div className="appointmentDesign">
         Appointment   
