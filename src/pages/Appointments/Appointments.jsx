14bfe85eba7a1652270a5751bc559a4eed968eac
martaguillemolmos <martetaguillem@gmail.com>
2023-11-16 18:08:50 +0100

    feat: Retrieve the user's appointments and view them through the console.log.

--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from "react-router-dom";
 import "./Appointments.css"
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getAppointmentsByUserId } from "../../services/apiCalls";
 
 
 export const Appointments = () => {
@@ -10,17 +11,30 @@ export const Appointments = () => {
     const navigate = useNavigate();
 
     // Instanciamos Redux en lectura
-     const rdxToken = useSelector(userData);
+    const rdxToken = useSelector(userData);
+    //Aquí recuperamos solo el token
+    const token = rdxToken.credentials.token
+    
+    const [appointments, setAppointments] = useState ([])
 
       //Utilizamos este useEffect para que, en el caso que alguien ya se haya logeado, no pueda acceder a esta vista.
         useEffect(()=>{
         //Comprobamos si ya hay un token almacenado en Redux
-        if(!rdxToken?.credentials.token){
-          console.log(rdxToken)
-          //Si ya contamos con un token, redirigimos al usuario a inicio.
+        if(token){
+        //Comprobamos que el array de appointments sea menor a 0.
+         if(appointments.length === 0){
+            //Entonces, nos solicitamos a través del token las citas de dicho usuario.
+            getAppointmentsByUserId(token)
+            .then ((results) => {
+                setAppointments(results.data)
+            })
+            .catch ((error) => console.log (error))
+         }
+        } else {
+        //Si no contamos con un token, redirigimos al usuario a login.
           navigate("/login");
-        } 
-      },[rdxToken, navigate]);
+        }
+      },[appointments]);
 
     return (
         <div className="appointmentDesign">
