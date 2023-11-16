6b5c6f01da25d93e7dbcaf19c7903f1e3bc05c8a
martaguillemolmos <martetaguillem@gmail.com>
2023-11-16 18:36:25 +0100

    feat:Retrieve all products and store them and view them in the console.log

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
 import "./Products.css"
+import { getAllproducts } from "../../services/apiCalls"
+
 
 export const Products = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect (() => {
+        if (products.length === 0){
+        getAllproducts()
+        .then ((results) => {
+            setProducts(results.data)
+            console.log(results.data)
+        })
+        .catch((error) => console.og (error))
+        }
+    }, [products])
+
     return (
-        <div className="profileDesign">
-        Products
-        </div>
+    <div className="profileDesign">
+    Products
+    </div>
     )
 }
