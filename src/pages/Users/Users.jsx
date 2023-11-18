e3c20f8bcb52156b3c38009a2c39877778384c70
martaguillemolmos <martetaguillem@gmail.com>
2023-11-18 19:45:41 +0100

    feat: Create the CardUsers document, where we will store the information of all registered users.

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,13 +1,17 @@
 import { useSelector } from "react-redux"
 import { userData } from "../userSlice";
 import { jwtDecode } from "jwt-decode";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getAllUsers } from "../../services/apiCalls";
 
 export const Users = () => {
     const navigate = useNavigate();
-
     const rdxToken = useSelector(userData);
+    const [users, setUsers] = useState([]);
+    const [msgError, setMsgError] = useState("");
+
+
 
     useEffect (() => {
         if(rdxToken !== ""){
@@ -15,7 +19,21 @@ export const Users = () => {
             const decoredToken = jwtDecode(token)
             console.log(decoredToken)
             if (decoredToken.role == "super_admin"){
-            console.log("eje")
+                
+                getAllUsers(token)
+                .then((results) => {
+                    setUsers(results.data);
+      
+                  })
+                  .catch((error) => {
+                    if (error.response && error.response.data) {
+                      // Si tenemos un mensaje en response.data, lo mostramos
+                      setMsgError(error.response.data);
+                    } else {
+                      // Si no tenemos un mensaje en response.data
+                      setMsgError("Hubo un error al cargar las citas.");
+                    }
+                  });
             } else {
                 navigate ("/")
             }
@@ -24,6 +42,26 @@ export const Users = () => {
         }
        }, [rdxToken, navigate])
     return(
-        <>Users</>
+        <div className="usersDesign">
+      {users.length > 0 ? (
+        <div className="usersRoster">
+          {users.map((appointments) => {
+            return (
+              <CardAppointments
+                key={appointments.id}
+                client={appointments.client}
+                artist={appointments.artist}
+                portfolio={appointments.portfolio_id}
+                date={appointments.date}
+                status_appointment={appointments.status_appointment}
+                is_active={appointments.is_active}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div>{msgError}</div>
+      )}
+      </div>
     )
 }
